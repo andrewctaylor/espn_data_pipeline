fdad68fdf6311a538cb9bec1445ab104f8248486
Andrew Taylor <andrewtaylor@berkeley.edu>
2025-08-14 19:12:34 -0700

    images table completed

--- espn_dbt/models/staging/articles.sql
+++ espn_dbt/models/staging/articles.sql
@@ -2,18 +2,19 @@
 
 WITH articles_cte AS (
   SELECT 
+      article_object:id::INT                      AS article_id,
       article_object:byline::STRING               AS byline,
       article_object:categories                   AS categories,
       article_object:dataSourceIdentifier::STRING AS dataSourceIdentifier,
       article_object:description::STRING          AS description,
       article_object:headline::STRING             AS headline,
-      article_object:id::INT                      AS article_id,
-      article_object:images                       AS images,
       article_object:lastModified::TIMESTAMP_NTZ  AS lastModified,
+      league,
       article_object:links                        AS links,
       article_object:nowId::STRING                AS nowId,
       article_object:premium::BOOLEAN             AS premium,
       article_object:published::TIMESTAMP_NTZ     AS published,
+      sport,                                       
       article_object:type::STRING                 AS type
   FROM {{ ref('articles_raw') }}
 )
---
+++ espn_dbt/models/staging/images.sql
@@ -0,0 +1,27 @@
+{{ config(materialized='table') }}
+
+WITH src AS (
+    SELECT
+        article_object:id::INT AS article_id,
+        article_object:images  AS images
+    FROM {{ ref('articles_raw') }}
+    WHERE article_object:id IS NOT NULL
+),
+
+images_cte AS (
+    SELECT
+        images[0]:id::STRING as image_id,
+        article_id,
+        images[0]:alt::STRING as alt,
+        images[0]:caption::STRING as caption,
+        images[0]:credit::STRING as credit,
+        images[0]:height::INT as height,
+        images[0]:dataSourceIdentifier::STRING as imagedataSourceIdentifier,
+        images[0]:name::STRING as name,
+        images[0]:type::STRING as type,
+        images[0]:url::STRING as url,
+        images[0]:width::INT as width   
+    FROM src
+)
+
+SELECT DISTINCT * FROM images_cte
